647c28ea59afe2e0810d54d2dcd08b107dd7c94a
Dante Tucker <dante@dantetucker.com>
2024-04-01 09:08:56 -0400

    First Commit

---
+++ NoAprilFools/Class1.cs
@@ -0,0 +1,40 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using ResoniteModLoader;
+
+namespace NoAprilFools
+{
+    public class NoAprilFools : ResoniteMod
+    {
+        public override string Name => "NoAprilFools";
+        public override string Author => "Dante";
+        public override string Version => "1.0.0";
+        public override string Link => "none";
+
+        public static ModConfiguration Config;
+
+        public override void OnEngineInit()
+        {
+            Config = GetConfiguration();
+            SetupMod();
+        }
+
+        static void SetupMod()
+        {
+            Harmony harmony = new Harmony("U-Dante.NoAprilFools");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Engine))]
+        private static class EnginePatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Engine), "get_IsAprilFools")]
+            private static bool IsAprilFoolsPrefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+    }
+}
